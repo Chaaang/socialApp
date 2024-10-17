68b030f1c9219cc26ed36ba74c1bcf89598ba982
John Mar Chang <johnmarchang18@gmail.com>
2024-10-18 04:29:56 +0800

    Auth
    Log in / Register pages

---
+++ social_app/lib/features/auth/presentation/cubits/auth_cubit.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_app/features/auth/domain/entities/app_user.dart';
+import 'package:social_app/features/auth/domain/repos/auth.repo.dart';
+import 'package:social_app/features/auth/presentation/cubits/auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  final AuthRepo authRepo;
+
+  AppUser? _currentUser;
+
+  AuthCubit({required this.authRepo}) : super(AuthInitial());
+
+  //check if the user is already authenticated
+  void checkAuth() async {
+    final AppUser? user = await authRepo.getCurrentUser();
+    if (user != null) {
+      _currentUser = user;
+      emit(Authenticated(user));
+    } else {
+      emit(UnAuthenticated());
+    }
+  }
+
+  //get current user
+  AppUser? get currentUser => _currentUser;
+
+  //login with email and pw
+  Future<void> login(String email, password) async {
+    try {
+      emit(AuthLoading());
+      final user = await authRepo.loginWithEmailPassword(email, password);
+
+      if (user != null) {
+        _currentUser = user;
+        emit(Authenticated(user));
+      } else {
+        emit(UnAuthenticated());
+      }
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(UnAuthenticated());
+    }
+  }
+
+  Future<void> register(email, password) async {
+    try {
+      emit(AuthLoading());
+      final user =
+          await authRepo.registerWithEmailPassword(email, password, '');
+
+      if (user != null) {
+        _currentUser = user;
+        emit(Authenticated(user));
+      } else {
+        emit(UnAuthenticated());
+      }
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(UnAuthenticated());
+    }
+  }
+
+  Future<void> logout() async {
+    await authRepo.logout();
+    emit(UnAuthenticated());
+  }
+}
---
+++ social_app/lib/features/auth/presentation/pages/login.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/features/auth/presentation/components/my_text_field.dart';
+import '../components/my_button.dart';
+
+class LoginPage extends StatefulWidget {
+  final void Function()? togglePages;
+  const LoginPage({super.key, required this.togglePages});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordControoler = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //LOGO
+                Icon(
+                  Icons.lock_open_rounded,
+                  size: 80,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+
+                const SizedBox(
+                  height: 50,
+                ),
+                //WELCOME MESSAGE
+                Text(
+                  'Welcome back, you\'ve been missed!',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                // EMAIL TEXTFIELD
+                MyTextField(
+                    controller: emailController,
+                    obscureText: false,
+                    hintText: 'Email'),
+                //PASSWORD TEXTFIELD
+                const SizedBox(height: 10),
+                MyTextField(
+                    controller: passwordControoler,
+                    obscureText: true,
+                    hintText: 'Password'),
+                //BUTTON
+                const SizedBox(height: 25),
+                MyButton(
+                  onTap: () {
+                    print('test');
+                  },
+                  text: 'Sign In',
+                ),
+                // Message to register
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an Account?',
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    const SizedBox(width: 5),
+                    GestureDetector(
+                        onTap: widget.togglePages,
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontWeight: FontWeight.bold),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
